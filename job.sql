9d3d164ef2242544ae2dff0655ac799e84aa637a
Bilker2 <58659211+Bilker2@users.noreply.github.com>
2022-05-04 09:09:09 +0300

    Update job.sql

--- job.sql
+++ job.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Job(
-    J_Id number CONSTRAINT J_Id PRIMARY KEY,
+    ID number CONSTRAINT Job_Id PRIMARY KEY,
     Education VARCHAR2(255),
     Employment_tpye VARCHAR2(25) NOT NULL,
     Skill VARCHAR2(255),
-    Job_dec VARCHAR2(255) ,
-    Job_Title VARCHAR2(55) NOT NULL,
-    Job_Location VARCHAR2(255),
-    Employer_Id number CONSTRAINT Empler_FK REFERENCES Employer(Emp_Id) ON DELETE CASCADE,
+    Description VARCHAR2(255) ,
+    tital VARCHAR2(55) NOT NULL,
+    Location VARCHAR2(255),
+    Emp_ID number CONSTRAINT Employer_FK REFERENCES Employer(ID) ON DELETE CASCADE,
     Num_Of_Emploees number NOT NULL
 );
