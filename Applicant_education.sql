e37d2b91b56304b3b79a3dd04adc97892101c0e1
Bilker2 <58659211+Bilker2@users.noreply.github.com>
2022-05-04 09:16:04 +0300

    Update Applicant_education.sql

--- Applicant_education.sql
+++ Applicant_education.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Applicant_education(
-    S_Id number PRIMARY KEY,
-    APP_Id NUMBER CONSTRAINT Edu_FK REFERENCES applier(App_Id) ON DELETE CASCADE,
-    Elocation VARCHAR2(55) NOT NULL,
+    ID number PRIMARY KEY,
+    APP_Id NUMBER CONSTRAINT Edu_FK REFERENCES Applicant(ID) ON DELETE CASCADE,
+    Location VARCHAR2(55) NOT NULL,
     Major VARCHAR2(24) NOT NULL,
     Grade float NOT NULL,
     Sdata DATE NOT NULL,
